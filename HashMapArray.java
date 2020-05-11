2840385ad6e2d50ce6a226958dce130a937de840
Joachim <jj@jj22.de>
2020-05-11 11:39:30 +0000

    asd

--- HashMapArray.java
+++ HashMapArray.java
@@ -1,3 +1,20 @@
+import java.util.HashMap;
+
 public class HashMapArray {
+	private static HashMap<String, Pair<Double, Integer>> GRADEOVERVIEW;
 	
+	public Pair<Double, Integer> addTestResult(String lectureame, Pair<Double,Integer> gradeAndECTS){
+
+	}
+	public void currentECTS(){
+
+	}
+
+	public Optional<Pair<Double,Integer>>getExamResults(){
+
+	}
+
+	public double totalGradeAverage(){
+		
+	}
 }

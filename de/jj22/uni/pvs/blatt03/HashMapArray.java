d12fae48a83b485afe7b68418cbf3fd78d561c97
Joachim <jj@jj22.de>
2020-05-11 19:50:56 +0200

    asd

--- de/jj22/uni/pvs/blatt03/HashMapArray.java
+++ de/jj22/uni/pvs/blatt03/HashMapArray.java
@@ -1,22 +1,25 @@
 package de.jj22.uni.pvs.blatt03;
 
 import java.util.HashMap;
+import java.util.Optional;
+
+import de.jj22.uni.pvs.blatt02.Pair;
 
 public class HashMapArray {
 	private static HashMap<String, Pair<Double, Integer>> GRADEOVERVIEW;
 	
 	public Pair<Double, Integer> addTestResult(String lectureame, Pair<Double,Integer> gradeAndECTS){
-
+		return null;
 	}
 	public void currentECTS(){
 
 	}
 
 	public Optional<Pair<Double,Integer>>getExamResults(){
-
+		return null;
 	}
 
 	public double totalGradeAverage(){
-		
+		return 0;
 	}
 }

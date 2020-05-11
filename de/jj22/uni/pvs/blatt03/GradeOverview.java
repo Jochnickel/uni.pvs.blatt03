59c157bb8eadcb1009d2021fb2d6043cf506a478
Joachim <jj@jj22.de>
2020-05-12 00:11:15 +0200

    done

--- de/jj22/uni/pvs/blatt03/GradeOverview.java
+++ de/jj22/uni/pvs/blatt03/GradeOverview.java
@@ -6,22 +6,44 @@ import java.util.Optional;
 import de.jj22.uni.pvs.blatt02.Pair;
 
 public class GradeOverview {
-    private static HashMap<String, Pair<Double, Integer>> GRADEOVERVIEW;
+    final private HashMap<String, Pair<Double, Integer>> GRADEOVERVIEW = new HashMap<String, Pair<Double, Integer>>();
     
     
-    public Pair<Double, Integer> addTestResult(String lectureame, Pair<Double,Integer> gradeAndECTS){
-		return null;
-	}
+    public Pair<Double, Integer> addTestResult(final String lectureName, final Pair<Double, Integer> gradeAndECTS) {
+        // if (null==lectureName) throw new IllegalArgumentException();
+        // if (null==gradeAndECTS) throw new IllegalArgumentException();
 
-	public void currentECTS(){
+        return GRADEOVERVIEW.put(lectureName, gradeAndECTS);
+    }
 
-	}
+    public int currentECTS() {
+        // int ectsSum = 0;
+        // for (Pair<?,Integer> grade : GRADEOVERVIEW.values()) {
+        // ectsSum += grade.getSecond();
+        // }
+        // return ectsSum;
 
-	public Optional<Pair<Double,Integer>>getExamResults(){
-		return null;
+        return GRADEOVERVIEW.values().stream().mapToInt(grade -> grade.getSecond()).sum();
+    }
+
+    public Optional<Pair<Double, Integer>> getExamResults(final String lectureName) {
+        // if (null==lectureName) throw new IllegalArgumentException();
+
+        if (GRADEOVERVIEW.containsKey(lectureName)){
+            return Optional.ofNullable(GRADEOVERVIEW.get(lectureName));
+        } else {
+            return Optional.empty();
+        }
 	}
 
 	public double totalGradeAverage(){
-		return 0;
-	}
+        // double weightedGradeSum = 0;
+        // for (Pair<Double,Integer> grade : GRADEOVERVIEW.values()) {
+        //     weightedGradeSum += grade.getFirst()*grade.getSecond();
+        // }
+        // final double avgGrade = weightedGradeSum/currentECTS();
+        // return avgGrade;
+
+        return GRADEOVERVIEW.values().stream().mapToDouble(grade->grade.getFirst()*grade.getSecond()).sum()/currentECTS();
+    }
 }
--- de/jj22/uni/pvs/blatt03/Main.java
+++ de/jj22/uni/pvs/blatt03/Main.java
@@ -1,11 +1,16 @@
 package de.jj22.uni.pvs.blatt03;
 
+import de.jj22.uni.pvs.blatt02.Pair;
+
 public class Main {
     public static void main(String[] args) {
-        HashMapArray<String> hm1 = new HashMapArray<String>(5);
-        System.out.println(hm1.getLength());
-        hm1.setEntry(1, "asdsad");
-        HashMapArray<String> hm2 = new HashMapArray<String>(12);
-        char[] asd = new char[12];
+        GradeOverview go = new GradeOverview();
+        go.addTestResult("HM1", new Pair<>(4.0,10));
+        go.addTestResult("HM2", new Pair<>(4.0,10));
+        go.addTestResult("HM3", new Pair<>(4.0,10));
+        go.addTestResult("PVS", new Pair<>(1.0,8));
+        go.addTestResult("EIDI1", new Pair<>(1.1,6));
+        go.addTestResult("EIDI2", new Pair<>(1.1,6));
+        System.out.println(go.totalGradeAverage());
     }
 }

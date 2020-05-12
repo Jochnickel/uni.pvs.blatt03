605337cff70614c6b60887b83b90ad11923c6e12
Joachim <jj@jj22.de>
2020-05-12 12:24:39 +0200

    beautified

--- de/jj22/uni/pvs/blatt03/GradeOverview.java
+++ de/jj22/uni/pvs/blatt03/GradeOverview.java
@@ -7,8 +7,7 @@ import de.jj22.uni.pvs.blatt02.Pair;
 
 public class GradeOverview {
     final private HashMap<String, Pair<Double, Integer>> GRADEOVERVIEW = new HashMap<String, Pair<Double, Integer>>();
-    
-    
+
     public Pair<Double, Integer> addTestResult(final String lectureName, final Pair<Double, Integer> gradeAndECTS) {
         // if (null==lectureName) throw new IllegalArgumentException();
         // if (null==gradeAndECTS) throw new IllegalArgumentException();
@@ -29,21 +28,22 @@ public class GradeOverview {
     public Optional<Pair<Double, Integer>> getExamResults(final String lectureName) {
         // if (null==lectureName) throw new IllegalArgumentException();
 
-        if (GRADEOVERVIEW.containsKey(lectureName)){
+        if (GRADEOVERVIEW.containsKey(lectureName)) {
             return Optional.ofNullable(GRADEOVERVIEW.get(lectureName));
         } else {
             return Optional.empty();
         }
-	}
+    }
 
-	public double totalGradeAverage(){
+    public double totalGradeAverage() {
         // double weightedGradeSum = 0;
         // for (Pair<Double,Integer> grade : GRADEOVERVIEW.values()) {
-        //     weightedGradeSum += grade.getFirst()*grade.getSecond();
+        // weightedGradeSum += grade.getFirst()*grade.getSecond();
         // }
         // final double avgGrade = weightedGradeSum/currentECTS();
         // return avgGrade;
 
-        return GRADEOVERVIEW.values().stream().mapToDouble(grade->grade.getFirst()*grade.getSecond()).sum()/currentECTS();
+        return GRADEOVERVIEW.values().stream().mapToDouble(grade -> grade.getFirst() * grade.getSecond()).sum()
+                / currentECTS();
     }
 }
--- de/jj22/uni/pvs/blatt03/Main.java
+++ de/jj22/uni/pvs/blatt03/Main.java
@@ -5,12 +5,12 @@ import de.jj22.uni.pvs.blatt02.Pair;
 public class Main {
     public static void main(String[] args) {
         GradeOverview go = new GradeOverview();
-        go.addTestResult("HM1", new Pair<>(4.0,10));
-        go.addTestResult("HM2", new Pair<>(4.0,10));
-        go.addTestResult("HM3", new Pair<>(4.0,10));
-        go.addTestResult("PVS", new Pair<>(1.0,8));
-        go.addTestResult("EIDI1", new Pair<>(1.1,6));
-        go.addTestResult("EIDI2", new Pair<>(1.1,6));
+        go.addTestResult("HM1", new Pair<>(4.0, 10));
+        go.addTestResult("HM2", new Pair<>(4.0, 10));
+        go.addTestResult("HM3", new Pair<>(4.0, 10));
+        go.addTestResult("PVS", new Pair<>(1.0, 8));
+        go.addTestResult("EIDI1", new Pair<>(1.1, 6));
+        go.addTestResult("EIDI2", new Pair<>(1.1, 6));
         System.out.println(go.totalGradeAverage());
     }
 }
